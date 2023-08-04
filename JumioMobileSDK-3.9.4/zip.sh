e99b43a22a24f7aa99cc484ecccf6bdb699b2910
Chris So <chris.so@crypto.com>
2023-08-04 14:51:53 +0800

    add JumioMobileSDK-3.9.4

---
+++ JumioMobileSDK-3.9.4/zip.sh
@@ -0,0 +1,15 @@
+mkdir -p zips
+
+cd xcframeworks
+
+zip -r -X ../zips/BAMCheckout.zip BAMCheckout.xcframework
+zip -r -X ../zips/DocumentVerification.zip DocumentVerification.xcframework
+zip -r -X ../zips/iProov.zip iProov.xcframework
+zip -r -X ../zips/JumioCore.zip JumioCore.xcframework
+zip -r -X ../zips/JumioIProov.zip JumioIProov.xcframework
+zip -r -X ../zips/JumioNFC.zip JumioNFC.xcframework
+zip -r -X ../zips/Microblink.zip Microblink.xcframework
+zip -r -X ../zips/Netverify.zip Netverify.xcframework
+zip -r -X ../zips/NetverifyBarcode.zip NetverifyBarcode.xcframework
+zip -r -X ../zips/SocketIO.zip SocketIO.xcframework
+zip -r -X ../zips/Starscream.zip Starscream.xcframework
